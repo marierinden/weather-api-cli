4c279d1c4f45483f32b9fe9a873abc33e8f4ce72
marie <marie@shoppingnorge.no>
2025-01-20 13:51:44 +0100

    beregner hvor lenge til solen går ned

--- cli/astronomy.php
+++ cli/astronomy.php
@@ -20,6 +20,24 @@ $city = $location['name'];
 $sunrise = $astronomy['astro']['sunrise'];
 $sunset = $astronomy['astro']['sunset'];
 
-$output = sprintf('In %s the sun sets %s and rises %s', $city, $sunset, $sunrise);
+$timezone = new DateTimeZone('Europe/Oslo');
+$now = new DateTime('now', $timezone);
+$sunsetTime = Datetime::createFromFormat('H:i A', $sunset, $timezone);
+
+if ($now > $sunsetTime) {
+    $sunsetTime->modify('+1 day');
+}
+
+$timeUntilSunset = $now->diff($sunsetTime);
+
+$output = sprintf(
+    'In %s the sun sets at %s and rises at %s. The current time is %s. Time until sunset: %s hours and %s minutes.',
+    $city,
+    $sunset,
+    $sunrise,
+    $now->format('H:i'),
+    $timeUntilSunset->h,
+    $timeUntilSunset->i
+);
 
 var_dump($output);
