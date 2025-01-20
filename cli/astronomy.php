e73a17a729ea3e5616695b1609bff5eed2e4a356
fredrik-tveraaen <fredrik.tveraaen@apiera.io>
2025-01-20 14:42:20 +0100

    Commit

--- cli/astronomy.php
+++ cli/astronomy.php
@@ -1,6 +1,7 @@
 <?php
 
 use GuzzleHttp\Client;
+use Marie\WeatherApp\Exception\AstronomyException;
 use Marie\WeatherApp\Service\AstronomyService;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -9,35 +10,21 @@ $dotEnvPath = __DIR__ . '/../.env.local';
 $env = parse_ini_file($dotEnvPath);
 $apiKey = $env['WEATHER_API_KEY'];
 
+$options = getopt('', ['city:']);
+
 $client = new Client();
 $astronomyService = new AstronomyService($client, $apiKey);
-$astronomyDataInOslo = $astronomyService->getAstronomyByCity('oslo');
 
-$location = $astronomyDataInOslo->getLocation();
-$astronomy = $astronomyDataInOslo->getAstronomy();
+try {
+    echo $astronomyService->getConsoleOutput($options['city']);
+} catch (AstronomyException $exception) {
+    if ($exception->getResponse()) {
+        $responseBody = json_decode($exception->getResponse()->getBody()->getContents(), true);
 
-$city = $location['name'];
-$sunrise = $astronomy['astro']['sunrise'];
-$sunset = $astronomy['astro']['sunset'];
-
-$timezone = new DateTimeZone('Europe/Oslo');
-$now = new DateTime('now', $timezone);
-$sunsetTime = Datetime::createFromFormat('H:i A', $sunset, $timezone);
-
-if ($now > $sunsetTime) {
-    $sunsetTime->modify('+1 day');
+        echo sprintf(
+            'Received code %d and message %s from API',
+            $responseBody['error']['code'],
+            $responseBody['error']['message']
+        );
+    }
 }
-
-$timeUntilSunset = $now->diff($sunsetTime);
-
-$output = sprintf(
-    'In %s the sun sets at %s and rises at %s. The current time is %s. Time until sunset: %s hours and %s minutes.',
-    $city,
-    $sunset,
-    $sunrise,
-    $now->format('H:i'),
-    $timeUntilSunset->h,
-    $timeUntilSunset->i
-);
-
-var_dump($output);
--- src/Service/AstronomyService.php
+++ src/Service/AstronomyService.php
@@ -2,8 +2,13 @@
 
 namespace Marie\WeatherApp\Service;
 
+use DateMalformedStringException;
+use DateTime;
+use DateTimeZone;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use Marie\WeatherApp\Exception\AstronomyException;
 use Marie\WeatherApp\Interface\ServiceInterface;
 use Marie\WeatherApp\Model\AstronomyModel;
 use Psr\Http\Message\ResponseInterface;
@@ -21,26 +26,77 @@ class AstronomyService implements ServiceInterface
     /**
      * @param array $params
      * @return ResponseInterface
-     * @throws GuzzleException
+     * @throws AstronomyException
      */
     public function request(array $params = []): ResponseInterface
     {
         $params = array_merge($params, ['key' => $this->apiKey]);
         $endpoint = sprintf('%s%s',self::BASE_URL, '/astronomy.json');
-        return $this->client->get($endpoint, [
-            'query' => $params,
-        ]);
+
+        try {
+            return $this->client->get($endpoint, [
+                'query' => $params,
+            ]);
+        } catch (GuzzleException $exception) {
+            if ($exception instanceof RequestException) {
+                throw new AstronomyException($exception->getMessage(), $exception->getResponse());
+            }
+
+            throw new AstronomyException($exception->getMessage());
+        }
+
     }
 
     /**
      * @param string $city
      * @return AstronomyModel
-     * @throws GuzzleException
+     * @throws AstronomyException
      */
     public function getAstronomyByCity(string $city): AstronomyModel
     {
         $response = $this->request(['q' => $city]);
         $astronomyData = json_decode($response->getBody()->getContents(), true);
         return new AstronomyModel($astronomyData);
+    }
+
+    /**
+     * @param string $city
+     * @return string
+     * @throws AstronomyException
+     */
+    public function getConsoleOutput(string $city): string
+    {
+        $astronomyData = $this->getAstronomyByCity($city);
+
+        $location = $astronomyData->getLocation();
+        $astronomy = $astronomyData->getAstronomy();
+
+        $city = $location['name'];
+        $sunrise = $astronomy['astro']['sunrise'];
+        $sunset = $astronomy['astro']['sunset'];
+
+        try {
+            $timezone = new DateTimeZone('Europe/Oslo');
+            $now = new DateTime('now', $timezone);
+            $sunsetTime = Datetime::createFromFormat('H:i A', $sunset, $timezone);
+
+            if ($now > $sunsetTime) {
+                $sunsetTime->modify('+1 day');
+            }
+        } catch (DateMalformedStringException $exception) {
+            throw new AstronomyException($exception->getMessage());
+        }
+
+        $timeUntilSunset = $now->diff($sunsetTime);
+
+        return sprintf(
+            'In %s the sun sets at %s and rises at %s. The current time is %s. Time until sunset: %s hours and %s minutes.',
+            $city,
+            $sunset,
+            $sunrise,
+            $now->format('H:i'),
+            $timeUntilSunset->h,
+            $timeUntilSunset->i
+        );
     }
 }
